e6f4289ff95e6f55dcbcf50edb9b40979aec8ebc
Shivam <tripathishivam9026@gmail.com>
2026-02-19 18:09:30 +0530

    Removed login register added enter page

--- app.py
+++ app.py
@@ -1,75 +1,35 @@
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, render_template, request, redirect
 import recommender
-import database
+import os
 
 app = Flask(__name__)
-app.secret_key = "secret123"
 
-database.create_tables()
+@app.route("/", methods=["GET", "POST"])
+def enter():
 
-@app.route("/")
-def home():
-    return redirect("/login")
-
-@app.route("/register", methods=["GET","POST"])
-def register():
     if request.method == "POST":
-        username = request.form["username"]
-        password = request.form["password"]
+        return redirect("/dashboard")
 
-        if database.register_user(username, password):
-            return redirect("/login")
+    return render_template("enter.html")
 
-    return render_template("register.html")
 
-@app.route("/login", methods=["GET","POST"])
-def login():
-    if request.method == "POST":
-        username = request.form["username"]
-        password = request.form["password"]
-
-        if database.login_user(username, password):
-            session["user"] = username
-            return redirect("/dashboard")
-
-    return render_template("login.html")
-
-@app.route("/dashboard", methods=["GET","POST"])
+@app.route("/dashboard", methods=["GET", "POST"])
 def dashboard():
-
-    if "user" not in session:
-        return redirect("/login")
 
     recommendations = []
 
     if request.method == "POST":
 
         interest = request.form["interest"]
-        results = recommender.recommend_courses(interest)
 
-        for course in results:
-            database.save_history(
-                session["user"],
-                interest,
-                course["Course Name"]
-            )
+        recommendations = recommender.recommend_courses(interest)
 
-        recommendations = results
+    return render_template("dashboard.html",
+                           recommendations=recommendations)
 
-    return render_template(
-        "dashboard.html",
-        user=session["user"],
-        recommendations=recommendations
-    )
-
-@app.route("/logout")
-def logout():
-    session.pop("user", None)
-    return redirect("/login")
-
-import os
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))  # Render provides PORT
-    app.run(host="0.0.0.0", port=port)
 
+    port = int(os.environ.get("PORT", 5000))
+
+    app.run(host="0.0.0.0", port=port)
